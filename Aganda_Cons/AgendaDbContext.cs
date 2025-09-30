3afc3c7410188ee6304cf2eaf99e2ad4d0a8bdaa
Waldo Heudens <waldo@heudens.be>
2025-09-30 12:09:46 +0200

    Voeg een class library toe (Agenda_Models) Voeg een eerste model toe aan Agenda_Models Voeg daaraan fundamentele data-annotatie toe Wijzig de Agenda_Cons projectfile om de library te gebruiken Voeg een eerste tabel toe aan Agenda_Cons in AgendaDbContext

--- Aganda_Cons/AgendaDbContext.cs
+++ Aganda_Cons/AgendaDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Agenda_Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace Agenda_Cons
 {
     internal class AgendaDbContext: DbContext
     {
+        public DbSet<Appointment> Appointments { get; set; } 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost;Database=AgendaDb;User Id=sa;Password=Your_password123;MultipleActiveResultSets=true");
---
+++ Agenda_Models/Appointment.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda_Models
+{
+    public class Appointment
+    {
+        private DateTime now = DateTime.Now + new TimeSpan(0,1,0);
+        private DateTime _from = DateTime.Now + new TimeSpan(1, 0, 0, 0);
+
+        public long Id { get; set; } // = -1;  Niet nodig, wordt automatisch gegenereerd    
+
+        [Required]
+        [Display(Name = "Vanaf")]
+        [DataType(DataType.DateTime)]
+        public DateTime From
+        {
+            get => _from;
+            set {if (value < now)
+                    _from = now;
+                else
+                    _from = value;
+            }
+        }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Tot")]
+        public DateTime To { get; set; } = DateTime.Now + new TimeSpan(1,1,30,0);
+
+        [Required]
+        [Display(Name = "Titel")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Omschrijving")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Hele dag")]
+        public bool AllDay { get; set; } = false;
+
+        [Required]
+        [Display(Name = "Aangemaakt")]
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        [Required]
+        [Display(Name = "Verwijderd")]
+        public DateTime Deleted { get; set; } = DateTime.MaxValue;
+    }
+}
