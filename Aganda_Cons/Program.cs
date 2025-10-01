09c13f66a16023b08a0ac899af1d85171107ebb8
Waldo Heudens <waldo@heudens.be>
2025-10-02 01:24:17 +0200

    Demonstratie - lambda-expressie - toevoegen en updaten van een object  in program.cs

--- Aganda_Cons/Program.cs
+++ Aganda_Cons/Program.cs
@@ -1,16 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Agenda_Cons;
+using Agenda_Models;
 
 using (var context = new AgendaDbContext())
 {
     AgendaDbContext.Seeder(context);
 
-    var appointments = context.Appointments.ToList();
+    // Haal alle appointments op en toon deze
+    var alleAppointments = context.Appointments;
+    Console.WriteLine("Alle afspraken: ");
+    foreach (var appointment in alleAppointments)
+    {
+        Console.WriteLine("- " + appointment);
+    }
+    
+
+    // Haal de appointments op die niet verwijderd zijn
+    // Gebruik Where met een gedelegeerde functie
+    var appointments = context.Appointments.Where(NotDeleted);
+    bool NotDeleted(Appointment a)
+    {
+        return a.Deleted > DateTime.Now;
+    }
+
+    // Doe exact hetzelfde met een anonieme delegate
+    appointments = context.Appointments
+                          .Where(delegate (Appointment a){return a.Deleted > DateTime.Now;});
+
+    // Doe weer exact hetzelfde met een lambda-expressie
+    appointments = context.Appointments.Where(a => a.Deleted > DateTime.Now);
+
+    // Toon de gefilterde appointments
+    Console.WriteLine("\nAlleen afspraken die niet verwijderd werden:");
     foreach (var appointment in appointments)
     {
         Console.WriteLine(appointment);
     }
+
+    // Toevoegen van een afspraak
+    Appointment nieuweAfspraak = new Appointment() { Title = "Tandardsbezoek", 
+                                                     Description = "Dat gaat pijn doen",
+                                                     From = DateTime.Now.AddDays(7)};
+    context.Add(nieuweAfspraak);
+    context.SaveChanges();
+    Console.WriteLine("\nEen afspraak werd toegevoegd:");
+    foreach (var appointment in appointments)
+    {
+        Console.WriteLine(appointment);
+    }
+
+    // Wijzigen van een afspraak
+    Appointment teWijzigen = appointments.FirstOrDefault(a => a.Title == "Tandardbezoek");
+    if (teWijzigen != null)
+    {
+        teWijzigen.From = DateTime.Now.AddDays(30);
+        teWijzigen.Title = "Doktersbezoek";
+        teWijzigen.Description = "Dat gaat minder pijn doen";
+        context.Update(teWijzigen);
+        context.SaveChanges();
+
+        Console.WriteLine("\nEen afspraak werd toegevoegd:");
+        foreach (var appointment in appointments)
+        {
+            Console.WriteLine(appointment);
+        }
+    }
+
+
+
 }
 
 
