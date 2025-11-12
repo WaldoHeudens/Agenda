114737a818ab6a210db72aa9e2dddbd9eabd9583
Waldo Heudens <waldo@heudens.be>
2025-11-12 21:13:39 +0100

    Terug naar originele situatie

---
+++ Agenda_App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Agenda_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Agenda_App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Agenda_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
