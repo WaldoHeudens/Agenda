90ad31433faa82ad74bdcbf612e4487457291b29
Waldo Heudens <waldo@heudens.be>
2025-11-26 17:30:44 +0100

    MAUI:  Complexere Controls - implementatie SwaipeView in MVVM

---
+++ Agenda_App/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Agenda_App.ViewModels;
+
+namespace Agenda_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(MainViewModel _viewModel)
+        {
+            InitializeComponent();
+            BindingContext = _viewModel;
+        }
+
+        private void Bewerkt_Invoked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
